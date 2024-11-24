a0cdb6d8b4878a3f1808e7d5db89b08941eefba7
Leandro Perrotta <leandro_perrotta@hotmail.com>
2024-11-24 13:20:48 -0300

    refactor(backend): improvements to bootstrap routines

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigurationModule } from "@http/modules/configuration/configuration.module";
+import { join } from "path";
+import { existsSync } from "fs";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
+    const isConfigMode = process.env.CONFIG_MODE === "true";
+    const envFileExists = existsSync(join(process.cwd(), ".env"));
+
+    if (isConfigMode && envFileExists) {
+        Logger.error(
+            "An .env file is detected. Configuration is not necessary. If you want wipe existing installation and start over again, please, remove all the databases and the .env file.",
+        );
+        process.exit();
+    } else if (!isConfigMode && !envFileExists) {
+        Logger.error(
+            "An .env file is not detected. Please execute `npm run configure` to setup the initial configuration.",
+        );
+        process.exit();
+    }
+
     const app = await NestFactory.create(
-        process.env.CONFIG_MODE === "true" ? ConfigurationModule : AppModule,
+        isConfigMode ? ConfigurationModule : AppModule,
     );
 
     app.enableCors({
